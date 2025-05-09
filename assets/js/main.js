a6d52782c03464cd12083f2a2217131c59d0b4b7
Jorge052011 <jimunozacuna+1@gmail.com>
2025-05-09 14:52:25 +0000

    se agregaron tooltip y configuración en js

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+console.log('jQuery versión:', $.fn.jquery);
+console.log('Bootstrap Modal existe?', typeof bootstrap.Modal);
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
